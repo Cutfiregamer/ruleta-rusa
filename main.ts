e79aa421296febc2b4d6b1b2ba9b121f4327dd5a
Cutfiregamer <133775047+Cutfiregamer@users.noreply.github.com>
2023-06-13 14:24:13 +0200

    MAs flechas y mas sonidos

--- main.ts
+++ main.ts
@@ -4,16 +4,20 @@ input.onButtonPressed(Button.A, function () {
         if (revolver == pos_bala) {
             basic.showIcon(IconNames.Skull)
             music.playMelody("C5 - C5 - C5 - C5 - ", 165)
+            basic.showArrow(ArrowNames.East)
         } else {
+            music.playTone(784, music.beat(BeatFraction.Half))
             basic.showIcon(IconNames.Heart)
         }
     } else {
+        music.playTone(494, music.beat(BeatFraction.Whole))
         basic.showArrow(ArrowNames.East)
     }
 })
 input.onButtonPressed(Button.B, function () {
     pos_bala = randint(1, 6)
     revolver = 0
+    music.playTone(247, music.beat(BeatFraction.Whole))
     basic.showArrow(ArrowNames.West)
 })
 let revolver = 0
